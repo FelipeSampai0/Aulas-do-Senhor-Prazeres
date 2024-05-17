62bdec6bcaa8a5be33279dbcc5510b0790cd94dc
Felsam <felipe.sampaio214@gmail.com>
2024-05-17 16:45:05 -0300

    1

--- src/ied007/Sorter.java
+++ src/ied007/Sorter.java
@@ -2,4 +2,27 @@ package ied007;
 
 public class Sorter {
 
+	public static int[] bubbleSort(int[] input) {
+		for (int i = 0; i < input.length - 1; i++) {
+			for (int j = 0; j < input.length - 1 - i; j++) {
+				if (input[j] > input[j + 1]) {
+					int temp = input[j];
+					input[j] = input[j + 1];
+					input[j + 1] = temp;
+				}
+			}
+		}
+		return input;
+	}
+
+	public static int[] selectionSort(int[] input) {
+
+		return input;
+	}
+
+	public static int[] insertionSort(int[] input) {
+
+		return input;
+	}
+
 }
--- src/ied007/Test.java
+++ src/ied007/Test.java
@@ -1,10 +1,22 @@
 package ied007;
 
+import java.util.Random;
+
+import ied007.Sorter;
+
 public class Test {
+	static Random random = new Random();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+
+	public static int[] randomArray(int size, int limit) {
+		int[] output = new int[size];
+		for (int i = 0; i < output.length; i++) {
+			output[i] = random.nextInt();
+		}
+		return output;
 	}
 
 }
